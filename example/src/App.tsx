1949eb9dcc6cba1f7c9e60b42c455a743c2a640e
aarkue <mail@aarkue.eu>
2023-06-02 20:33:27 +0200

    Add info to example app, updated styling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,18 @@
-import './App.css'
-import Tiptap from './TipTap'
+import "./App.css";
+import Tiptap from "./TipTap";
 
 function App() {
-
   return (
     <>
       <h1>TipTap + Math</h1>
-      <Tiptap/>
+      <p>
+        Visit{" "}
+        <a href="https://github.com/aarkue/tiptap-math-extension/">https://github.com/aarkue/tiptap-math-extension/</a>{" "}
+        for more information.
+      </p>
+      <Tiptap />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
