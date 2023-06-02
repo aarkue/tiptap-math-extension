ab95b7a06edd175c7d0325dcd62c0420181763b8
aarkue <mail@aarkue.eu>
2023-06-02 18:31:54 +0200

    Add gh-pages demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>TipTap + Math</h1>
+      <p>Visit <a href="https://github.com/aarkue/tiptap-math-extension/">https://github.com/aarkue/tiptap-math-extension/</a> for more information.</p>
       <Tiptap/>
     </>
   )
