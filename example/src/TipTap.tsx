ae2816618b5ba0fdb6b41e370187fb246290d3b9
aarkue <mail@aarkue.eu>
2024-04-09 23:45:47 +0200

    Expose tiptapEditor in demo window

--- example/src/TipTap.tsx
+++ example/src/TipTap.tsx
@@ -2,17 +2,24 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { MathExtension } from "../../packages/tiptap-math-extension/src/index";
 import "katex/dist/katex.min.css";
+import { useEffect } from "react";
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
-      MathExtension.configure({ evaluation: true, katexOptions: { macros: { "\\B": "\\mathbb{B}" } } }),
+      MathExtension.configure({ evaluation: false, katexOptions: { macros: { "\\B": "\\mathbb{B}" } } }),
     ],
     content: "<p>Hello World!</p>",
   });
 
-  return <EditorContent editor={editor} className="tiptap-editor" />;
+  useEffect(() => {
+    if(editor){
+      console.log({editor});
+      (window as any).tiptapEditor = editor;
+    }
+  },[editor])
+  return <EditorContent editor={editor} className="tiptap-editor"  />;
 };
 
 export default Tiptap;
